49b4cff8bed3ca9d7c771c2dbadeb3f2ec375437
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-02-10 14:00:53 +1100

    Allow points overlay

--- slice_view.py
+++ slice_view.py
@@ -1,22 +1,51 @@
+import numpy as np
 from matplotlib import pyplot as plt
+
+
+def _dscroll(key, axes):
+    volume = axes.volume
+    if key == 'down' or key == 'j':
+        shift = -1
+    elif key == 'up' or key == 'k':
+        shift = 1
+    axes.index = (axes.index + shift) % volume.shape[0]
+    axes.images[0].set_array(volume[axes.index])
+    if axes.points is not None:
+        update_points(axes)
+
 
 def process_key(event):
     fig = event.canvas.figure
     ax = event.inaxes or fig.axes[0]
-    volume = ax.volume
-    if event.key == 'down' or event.key == 'j':
-        shift = -1
-    elif event.key == 'up' or event.key == 'k':
-        shift = 1
-    ax.index = (ax.index + shift) % volume.shape[0]
-    ax.images[0].set_array(volume[ax.index])
+    if event.key in ['up', 'down', 'j', 'k']:
+        _dscroll(event.key, ax)
     fig.canvas.draw()
 
 
-def slice_view(volume, cmap=plt.cm.viridis):
+def slice_view(volume, points=None, pts_depth=2, cmap=plt.cm.viridis):
     fig, ax = plt.subplots()
     ax.volume = volume
+    ax.points = None
     ax.index = volume.shape[0] // 2
     ax.imshow(volume[ax.index], cmap=cmap)
+    if points is not None:
+        ax.points = points.T
+        ax.pts_depth = pts_depth
+        draw_points(ax)
     fig.canvas.mpl_connect('key_press_event', process_key)
     return fig, ax
+
+
+def draw_points(axes):
+    pln, row, col = axes.points
+    alpha = np.maximum(0, 1 - np.abs(pln - axes.index) / axes.pts_depth)
+    points_collection = [axes.scatter(x, y, alpha=a)
+                         for x, y, a in zip(col, row, alpha)]
+    axes.drawn_points = points_collection
+
+
+def update_points(axes):
+    pln = axes.points[0]
+    alpha = np.maximum(0, 1 - np.abs(pln - axes.index) / axes.pts_depth)
+    for pt, a in zip(axes.drawn_points, alpha):
+        pt.set_alpha(a)
