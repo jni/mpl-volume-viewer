4e3cf9809c74dd8228cfe6eaae967f6c20b05b56
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-02-14 10:19:15 +1100

    Initial code for an overlay

--- slice_view.py
+++ slice_view.py
@@ -2,6 +2,11 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
+def _random_cmap(n=256):
+    # to do: make this really map random colours
+    return plt.cm.Spectral(n)
+
+
 def _dscroll(key, axes):
     volume = axes.volume
     if key == 'down' or key == 'j':
@@ -10,8 +15,18 @@ def _dscroll(key, axes):
         shift = 1
     axes.index = (axes.index + shift) % volume.shape[0]
     axes.images[0].set_array(volume[axes.index])
+    if hasattr(axes, 'overlay'):
+        axes.images[1].set_array(axes.overlay[axes.index])
     if axes.points is not None:
         update_points(axes)
+
+
+def _toggle_overlay(ax):
+    if ax.overlay is not None:
+        ax = ax.images[1]
+        temp = ax.get_alpha()
+        ax.set_alpha(ax.old_alpha)
+        ax.old_alpha = temp
 
 
 def process_key(event):
@@ -19,10 +34,14 @@ def process_key(event):
     ax = event.inaxes or fig.axes[0]
     if event.key in ['up', 'down', 'j', 'k']:
         _dscroll(event.key, ax)
+    elif event.key == 'f':
+        _toggle_overlay(ax)
     fig.canvas.draw()
 
 
-def slice_view(volume, points=None, pts_depth=2, cmap=plt.cm.viridis):
+def slice_view(volume, cmap=plt.cm.gray,
+               points=None, pts_depth=2, pts_color='red',
+               labels=None, labels_cmap='random'):
     fig, ax = plt.subplots()
     ax.volume = volume
     ax.points = None
@@ -32,6 +51,12 @@ def slice_view(volume, points=None, pts_depth=2, cmap=plt.cm.viridis):
         ax.points = points.T
         ax.pts_depth = pts_depth
         draw_points(ax)
+    if labels is not None:
+        if labels_cmap == 'random':
+            cmap = _random_cmap(np.max(labels))
+        ax.overlay = cmap(labels)
+        ax.imshow(ax.overlay[ax.index], alpha=0.4)
+        ax.old_alpha = 0
     fig.canvas.mpl_connect('key_press_event', process_key)
     return fig, ax
 
